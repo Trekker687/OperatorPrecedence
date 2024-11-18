11e21928cb3b450b10f6d5bfdb2824780974dda0
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-18 17:25:26 +0530

    Add files via upload

---
+++ OperatorPrecedence/main.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number "))
+b = int(input("Enter a number "))
+c = int(input("Enter a number "))
+d = int(input("Enter a number "))
+e = (a+b) * c/d
+print(a,"+",b,"*",c,"/",d, "is", e)
+
+name = input("Enter your name")
+age = int(input("Enter your age"))
+
+if name == "Manvir" or name == "manvir" and age >= 10:
+    print(":)")
+else:
+    print(":(")
